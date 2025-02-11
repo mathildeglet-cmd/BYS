73e98e714a79b8d30ce9467c57ab5b35a4af0a9e
Mathilde Glet <you@example.com>
2025-02-11 19:39:54 +0100

    feat: added router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,32 +1,7 @@
 // Import necessary modules from React and React Router
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-
-/* ************************************************************************* */
-
-// Import the main app component
-import App from "./App";
-
-// Import additional components for new routes
-// Try creating these components in the "pages" folder
-
-// import About from "./pages/About";
-// import Contact from "./pages/Contact";
-
-/* ************************************************************************* */
-
-// Create router configuration with routes
-// You can add more routes as you build out your app!
-const router = createBrowserRouter([
-  {
-    path: "/", // The root path
-    element: <App />, // Renders the App component for the home page
-  },
-  // Try adding a new route! For example, "/about" with an About component
-]);
-
-/* ************************************************************************* */
+import Router from "./router";
 
 // Find the root element in the HTML document
 const rootElement = document.getElementById("root");
@@ -37,7 +12,7 @@ if (rootElement == null) {
 // Render the app inside the root element
 createRoot(rootElement).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Router />
   </StrictMode>,
 );
 
