c42a230b355403e0914196e6c58f37b2e226aba5
Mathilde Glet <you@example.com>
2025-02-14 09:31:09 +0100

    feat: getting back header  old version

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,7 +1,9 @@
 import { Menu } from "lucide-react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const navigate = useNavigate();
   const [isOpenMenu, setIsOpenMenu] = useState<boolean>(false);
 
   const handleIsOpenMenu = () => {
@@ -14,27 +16,40 @@ export default function Header() {
           <Menu />
         </button>
         {isOpenMenu && (
-          <section className="flex flex-col gap-2 mt-4 w-[8em] fixed bg-accent rounded-md p-2">
+          <section className="flex flex-col gap-2 mt-4 lg:w-[10em] fixed bg-accent rounded-md p-2">
             <a href="about">A propos</a>
             <a href="programs">Nos programmes</a>
-            <a href="contact_us">Nous contacter</a>
+            <a href="contact_us">Notre boutique</a>
           </section>
         )}
       </nav>
-      <img
-        src="../public/images/logo.jpg"
-        alt="une femme musclée"
-        className="w-[5em]"
-      />{" "}
+      <button
+        type="button"
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        <img
+          src="../public/images/logo.jpg"
+          alt="une femme musclée"
+          className="w-[5em]"
+        />
+      </button>{" "}
       <section className="flex flex-col lg:flex lg:flex-row gap-1 lg:gap-20 lg:mr-4">
         <button
           type="button"
+          onClick={() => {
+            navigate("/register");
+          }}
           className="border-solid border-1 border-slate-400 hover:bg-accent hover:border-none p-0.5 rounded-md lg:border-solid lg:border-1 lg:border-slate-400 lg:rounded-xl lg:h-[3em] lg:p-3 lg:hover:bg-accent lg:hover:border-none"
         >
           S'inscrire
         </button>
         <button
           type="button"
+          onClick={() => {
+            navigate("/login");
+          }}
           className="border-solid border-1 border-slate-400 hover:bg-accent hover:border-none p-1 rounded-md lg:border-solid lg:border-1 lg:border-slate-400 lg:rounded-xl lg:h-[3em] lg:p-3 lg:hover:bg-accent lg:hover:border-none"
         >
           Se connecter
